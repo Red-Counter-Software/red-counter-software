9bcedde44fa369847b5b2145a59c88d67009ad42
Matteo Mosca <matteo.mosca@gmail.com>
2019-05-06 12:25:15 +0200

    Add RedCounterSoftware.Validation project

---
+++ RedCounterSoftware.Common/Extensions/LambdaExpressionExtensions.cs
@@ -0,0 +1,97 @@
+﻿namespace RedCounterSoftware.Common.Extensions
+{
+    using System;
+    using System.Linq.Expressions;
+    using System.Reflection;
+
+    public static class LambdaExpressionExtensions
+    {
+        public static string GetPropertyName(this LambdaExpression selector)
+        {
+            MemberExpression me;
+
+            if (selector.Body is MemberExpression expression)
+            {
+                me = expression;
+            }
+            else
+            {
+                var op = ((UnaryExpression)selector.Body).Operand;
+                me = (MemberExpression)op;
+            }
+
+            return ((PropertyInfo)me.Member).Name;
+        }
+
+        public static Type GetPropertyType(this LambdaExpression selector)
+        {
+            MemberExpression me;
+
+            if (selector.Body is MemberExpression expression)
+            {
+                me = expression;
+            }
+            else
+            {
+                var op = ((UnaryExpression)selector.Body).Operand;
+                me = (MemberExpression)op;
+            }
+
+            return ((PropertyInfo)me.Member).PropertyType;
+        }
+
+        public static BinaryExpression CreateKeyComparisonExpression(this Expression leftExpression, Expression rightExpression)
+        {
+            if (leftExpression.Type == rightExpression.Type)
+            {
+                return Expression.Equal(leftExpression, rightExpression);
+            }
+
+            if (leftExpression.Type.IsNullableType())
+            {
+                rightExpression = Expression.Convert(rightExpression, leftExpression.Type);
+            }
+            else
+            {
+                leftExpression = Expression.Convert(leftExpression, rightExpression.Type);
+            }
+
+            return Expression.Equal(leftExpression, rightExpression);
+        }
+
+        public static ParameterExpression GetParameterExpression(this LambdaExpression expression)
+        {
+            var memberExp = expression.Body;
+            while (true)
+            {
+                if (memberExp is MemberExpression exp)
+                {
+                    switch (exp.Expression)
+                    {
+                        case ParameterExpression exp2:
+                            return exp2;
+                        case MemberExpression exp3:
+                            memberExp = exp3;
+                            continue;
+                    }
+                }
+
+                if (memberExp is UnaryExpression exp4)
+                {
+                    switch (exp4.Operand)
+                    {
+                        case ParameterExpression exp5:
+                            return exp5;
+                        case MemberExpression exp6:
+                            memberExp = exp6;
+                            continue;
+                    }
+                }
+
+                break;
+            }
+
+            throw new InvalidOperationException("No parameter expression found in provided expression");
+        }
+    }
+}
---
+++ RedCounterSoftware.Validation/FluentValidationValidator{T}.cs
@@ -0,0 +1,44 @@
+﻿namespace RedCounterSoftware.Validation
+{
+    using System;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Threading.Tasks;
+    using Common.Extensions;
+    using Common.Validation;
+    using FluentValidation;
+
+    public abstract class FluentValidationValidator<T> : AbstractValidator<T>, ICustomValidator<T>
+        where T : class
+    {
+        protected FluentValidationValidator() => this.CascadeMode = CascadeMode.StopOnFirstFailure;
+
+        public async Task<Result<T>> PerformValidation(T toValidate, int? index = null)
+        {
+            if (toValidate == null)
+            {
+                throw new ArgumentNullException(nameof(toValidate));
+            }
+
+            var result = await this.ValidateAsync(toValidate);
+            return new Result<T>(toValidate, result.Errors.Select(c => new Failure(c.PropertyName, c.ErrorMessage, c.AttemptedValue ?? string.Empty)).ToList(), index);
+        }
+
+        public Task<Result<T>> ValidateProperty<TK>(T toValidate, Expression<Func<T, TK>> propertySelector)
+        {
+            if (toValidate == null)
+            {
+                throw new ArgumentNullException(nameof(toValidate));
+            }
+
+            if (propertySelector == null)
+            {
+                throw new ArgumentNullException(nameof(propertySelector));
+            }
+
+            var result = this.Validate(toValidate);
+            return Task.FromResult(new Result<T>(toValidate, result.Errors.Where(e => e.PropertyName.StartsWith(propertySelector.GetPropertyName())).Select(c => new Failure(c.PropertyName, c.ErrorMessage, c.AttemptedValue ?? string.Empty))
+                    .ToList()));
+        }
+    }
+}
