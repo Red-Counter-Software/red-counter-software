18032d571c292d29ec7b2512a18acb5b61691fda
Matteo Mosca <matteo.mosca@gmail.com>
2019-05-06 12:50:12 +0200

    Add RedCounterSoftware.Mailing.SendGrid project

---
+++ RedCounterSoftware.Common/Mailing/FakeMailingService.cs
@@ -0,0 +1,19 @@
+﻿namespace RedCounterSoftware.Common.Mailing
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Common.Mailing;
+
+    public class FakeMailingService : IMailingService
+    {
+        public void Dispose()
+        {
+        }
+
+        public Task SendActivationEmail(string email, Guid activationGuid, string subject, string textBody, string htmlBody, CancellationToken cancellationToken = default) => Task.CompletedTask;
+
+        public Task SendPasswordRecoveryMail(string email, Guid passwordResetGuid, string subject, string textBody, string htmlBody, CancellationToken cancellationToken = default) => Task.CompletedTask;
+    }
+}
--- RedCounterSoftware.Common/Mailing/IMailingService.cs
+++ RedCounterSoftware.Common/Mailing/IMailingService.cs
@@ -14,17 +14,23 @@
         /// </summary>
         /// <param name="email">The email address of the user.</param>
         /// <param name="activationGuid">The activation guid.</param>
-        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <param name="subject">The subject of the mail.</param>
+        /// <param name="textBody">The body of the mail in plain text. Must contain two placeholders, the first for the base url, the second for the activation guid.</param>
+        /// <param name="htmlBody">The body of the mail in html. Must contain a placeholder for the activation url.</param>
+        /// /// <param name="cancellationToken">The cancellation token.</param>
         /// <returns>Returns a completed task.</returns>
-        Task SendActivationEmail(string email, Guid activationGuid, CancellationToken cancellationToken = default);
+        Task SendActivationEmail(string email, Guid activationGuid, string subject, string textBody, string htmlBody, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Sends a password recovery mail to the provided <see cref="User"/> containing a link to follow to generate a new password.
         /// </summary>
         /// <param name="email">The email address of the user.</param>
         /// <param name="passwordResetGuid">The password reset guid.</param>
+        /// /// <param name="subject">The subject of the mail.</param>
+        /// <param name="textBody">The body of the mail in plain text. Must contain two placeholders, the first for the base url, the second for the password reset guid.</param>
+        /// <param name="htmlBody">The body of the mail in html. Must contain a placeholder for the password reset url.</param>
         /// <param name="cancellationToken">The cancellation token.</param>
         /// <returns>Returns a completed task.</returns>
-        Task SendPasswordRecoveryMail(string email, Guid passwordResetGuid, CancellationToken cancellationToken = default);
+        Task SendPasswordRecoveryMail(string email, Guid passwordResetGuid, string subject, string textBody, string htmlBody, CancellationToken cancellationToken = default);
     }
 }
