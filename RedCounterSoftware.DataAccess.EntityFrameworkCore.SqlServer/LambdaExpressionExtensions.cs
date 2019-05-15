c149de02739a548a2a85ce12ed9606d5d5a1fe8c
Matteo Mosca <matteo.mosca@gmail.com>
2019-05-15 12:02:01 +0200

    Add RedCounterSoftware.DataAccess.EntityFrameworkCore.SqlServer project

---
+++ RedCounterSoftware.DataAccess.EntityFrameworkCore.SqlServer/LambdaExpressionExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace RedCounterSoftware.DataAccess.EntityFrameworkCore.SqlServer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Reflection;
+
+    using RedCounterSoftware.Common.Extensions;
+
+    public static class LambdaExpressionExtensions
+    {
+        public static Expression<Func<T, bool>> InExpression<T, TK>(
+            this Expression<Func<T, TK>> selector, IEnumerable<TK> array)
+        {
+            var p = selector.GetParameterExpression();
+            var contains = typeof(Enumerable).GetMethods(BindingFlags.Static | BindingFlags.Public).Where(c => c.Name == "Contains" && c.GetParameters().Length == 2).First().MakeGenericMethod(typeof(TK));
+            var body = Expression.Call(contains, Expression.Constant(array), selector.Body);
+            return Expression.Lambda<Func<T, bool>>(body, p);
+        }
+    }
+}
