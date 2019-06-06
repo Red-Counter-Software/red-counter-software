53c1fde7a434cf5ecedc338649a89fdb7b88a7f4
Matteo Mosca <matteo.mosca@gmail.com>
2019-06-06 17:01:42 +0200

    Add parameterless constructor to SearchParameters

--- RedCounterSoftware.Common/SearchParameters.cs
+++ RedCounterSoftware.Common/SearchParameters.cs
@@ -10,6 +10,10 @@
     /// <param name="currentPage">The 0 based index of the subset of items to return.</param>
     public class SearchParameters
     {
+        public SearchParameters()
+        {
+        }
+
         public SearchParameters(string searchTerm, string sortTerm, bool isDescending = false, int pageSize = 10, int currentPage = 0)
         {
             this.SearchTerm = searchTerm ?? string.Empty;
@@ -20,43 +24,43 @@
         }
 
         /// <summary>
-        /// Gets the term to search for.
+        /// Gets or sets the term to search for.
         /// </summary>
         /// <value>
         /// The term to search for.
         /// </value>
-        public string SearchTerm { get; }
+        public string SearchTerm { get; set; }
 
         /// <summary>
-        /// Gets the name of the property to sort by.
+        /// Gets or sets the name of the property to sort by.
         /// </summary>
         /// <value>
         /// The term to sort by.
         /// </value>
-        public string SortTerm { get; }
+        public string SortTerm { get; set; }
 
         /// <summary>
-        /// Gets a value indicating whether the order is descending or ascending.
+        /// Gets or sets a value indicating whether the order is descending or ascending.
         /// </summary>
         /// <value>
         /// True for descending order, false for ascending.
         /// </value>
-        public bool IsDescending { get; }
+        public bool IsDescending { get; set; }
 
         /// <summary>
-        /// Gets the amount of items to return.
+        /// Gets or sets the amount of items to return.
         /// </summary>
         /// <value>
         /// The amount of items to return.
         /// </value>
-        public int PageSize { get; }
+        public int PageSize { get; set; }
 
         /// <summary>
-        /// Gets the 0 based index of the subset of items to return.
+        /// Gets or sets the 0 based index of the subset of items to return.
         /// </summary>
         /// <value>
         /// The 0 based index of the subset of items to return.
         /// </value>
-        public int CurrentPage { get; }
+        public int CurrentPage { get; set; }
     }
 }
