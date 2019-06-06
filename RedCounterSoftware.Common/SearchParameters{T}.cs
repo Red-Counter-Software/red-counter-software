46e6d9fb7e5c92cad7413c7aa19ff321afe4d4c9
Matteo Mosca <matteo.mosca@gmail.com>
2019-06-06 13:22:35 +0200

    Add non generic SearchParameters

--- RedCounterSoftware.Common/SearchParameters{T}.cs
+++ RedCounterSoftware.Common/SearchParameters{T}.cs
@@ -8,7 +8,7 @@
     /// Defines standard search parameters to query an object store.
     /// </summary>
     /// <typeparam name="T">The type of entity to search.</typeparam>
-    public class SearchParameters<T>
+    public class SearchParameters<T> : SearchParameters
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="SearchParameters{T}"/> class.
@@ -19,21 +19,11 @@
         /// <param name="pageSize">The amount of items to return.</param>
         /// <param name="currentPage">The 0 based index of the subset of items to return.</param>
         public SearchParameters(string searchTerm, string sortTerm, bool isDescending = false, int pageSize = 10, int currentPage = 0)
+            : base(searchTerm, sortTerm, isDescending, pageSize, currentPage)
         {
-            this.SearchTerm = searchTerm ?? string.Empty;
             this.SortExpression = sortTerm.GetPropertyExpression<T>();
-            this.IsDescending = isDescending;
-            this.PageSize = pageSize >= 10 ? pageSize : 10;
-            this.CurrentPage = currentPage >= 0 ? currentPage : 0;
         }
 
-        /// <summary>
-        /// Gets the term to search for.
-        /// </summary>
-        /// <value>
-        /// The term to search for.
-        /// </value>
-        public string SearchTerm { get; }
 
         /// <summary>
         /// Gets the lambda expression indicating the property to sort by.
@@ -42,29 +32,5 @@
         /// The lambda expression indicating the property to sort by.
         /// </value>
         public Expression<Func<T, object>> SortExpression { get; }
-
-        /// <summary>
-        /// Gets a value indicating whether the order is descending or ascending.
-        /// </summary>
-        /// <value>
-        /// True for descending order, false for ascending.
-        /// </value>
-        public bool IsDescending { get; }
-
-        /// <summary>
-        /// Gets the amount of items to return.
-        /// </summary>
-        /// <value>
-        /// The amount of items to return.
-        /// </value>
-        public int PageSize { get; }
-
-        /// <summary>
-        /// Gets the 0 based index of the subset of items to return.
-        /// </summary>
-        /// <value>
-        /// The 0 based index of the subset of items to return.
-        /// </value>
-        public int CurrentPage { get; }
     }
 }
