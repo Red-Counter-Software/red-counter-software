b28ec47bd353cbf5a45ddcdf133e965d3d914d4f
Matteo Mosca <matteo.mosca@gmail.com>
2021-07-11 18:26:19 +0200

    Add DataContext{T} for DataAccess.Dapper.SqlServer

--- RedCounterSoftware.DataAccess.Dapper.SqlServer/ReadOnlyDataContext{T}.cs
+++ RedCounterSoftware.DataAccess.Dapper.SqlServer/ReadOnlyDataContext{T}.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Data.SqlClient;
     using System.Linq;
+    using System.Linq.Expressions;
     using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
@@ -14,12 +15,6 @@
     public abstract class ReadOnlyDataContext<T> : IReadDataContext<T>
         where T : RecordBase
     {
-        private readonly string tableName;
-
-        private readonly string schemaName;
-
-        protected SqlConnection SqlConnection { get; private set; }
-
         private bool disposedValue; // To detect redundant calls
 
         public ReadOnlyDataContext(string connectionString, string tableName, string schemaName = "dbo")
@@ -29,35 +24,41 @@
                 throw new ArgumentNullException(nameof(connectionString));
             }
 
-            this.tableName = !string.IsNullOrEmpty(tableName) ? tableName : throw new ArgumentNullException(nameof(tableName));
-            this.schemaName = schemaName;
+            this.TableName = !string.IsNullOrEmpty(tableName) ? tableName : throw new ArgumentNullException(nameof(tableName));
+            this.SchemaName = schemaName;
             this.SqlConnection = new SqlConnection(connectionString);
         }
 
+        protected string TableName { get; private set; }
+
+        protected string SchemaName { get; private set; }
+
+        protected SqlConnection SqlConnection { get; private set; }
+
         public virtual Task<int> Count(CancellationToken cancellationToken = default)
         {
-            var command = $"Select Count(*) From [{this.schemaName}].[{this.tableName}]";
+            var command = $"Select Count(*) From [{this.SchemaName}].[{this.TableName}]";
             return this.SqlConnection.ExecuteScalarAsync<int>(command);
         }
 
-        public virtual Task<bool> ExistsBy<TK>(System.Linq.Expressions.Expression<Func<T, TK>> selector, TK value, CancellationToken cancellationToken = default)
+        public virtual Task<bool> ExistsBy<TK>(Expression<Func<T, TK>> selector, TK value, CancellationToken cancellationToken = default)
         {
             var fieldName = selector.GetPropertyName();
-            var command = $"Select Count(Distinct 1) From [{this.schemaName}].[{this.tableName}] Where [{fieldName}] = @Value";
+            var command = $"Select Count(Distinct 1) From [{this.SchemaName}].[{this.TableName}] Where [{fieldName}] = @Value";
             return this.SqlConnection.ExecuteScalarAsync<bool>(command, new { Value = value });
         }
 
-        public virtual Task<T> GetBy<TK>(System.Linq.Expressions.Expression<Func<T, TK>> selector, TK value, CancellationToken cancellationToken = default)
+        public virtual Task<T> GetBy<TK>(Expression<Func<T, TK>> selector, TK value, CancellationToken cancellationToken = default)
         {
             var fieldName = selector.GetPropertyName();
-            var command = $"Select * From [{this.schemaName}].[{this.tableName}] Where [{fieldName}] = @Value";
+            var command = $"Select * From [{this.SchemaName}].[{this.TableName}] Where [{fieldName}] = @Value";
             return this.SqlConnection.QuerySingleOrDefaultAsync<T>(command, new { Value = value });
         }
 
-        public virtual async Task<SearchResult<T>> GetByMultipleValues<TK>(System.Linq.Expressions.Expression<Func<T, TK>> selector, TK[] values, CancellationToken cancellationToken = default)
+        public virtual async Task<SearchResult<T>> GetByMultipleValues<TK>(Expression<Func<T, TK>> selector, TK[] values, CancellationToken cancellationToken = default)
         {
             var fieldName = selector.GetPropertyName();
-            var command = $"Select * From [{this.schemaName}].[{this.tableName}] Where [{fieldName}] IN @Values";
+            var command = $"Select * From [{this.SchemaName}].[{this.TableName}] Where [{fieldName}] IN @Values";
             var result = await this.SqlConnection.QueryAsync<T>(command, new { Values = values }).ConfigureAwait(false);
             var items = result.ToArray();
             return new SearchResult<T>(items.Count(), items);
